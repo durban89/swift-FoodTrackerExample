223bef3f8a285cb2e4ee25b48da10b3a29eaa6be
durban.zhang <durban.zhang@gmail.com>
2017-05-22 11:08:56 +0800

    更新测试 TestCase

--- FoodTrackerExampleTests/FoodTrackerExampleTests.swift
+++ FoodTrackerExampleTests/FoodTrackerExampleTests.swift
@@ -10,27 +10,46 @@ import XCTest
 @testable import FoodTrackerExample
 
 class FoodTrackerExampleTests: XCTestCase {
+    // MARK: MealModel Class Tests
     
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    // Confirm that the Meal initializer returns a Meal object when passed valid parameters.
+    func testMealModelInitializationSucceeds(){
+        // Zero rating
+        let zeroRatingMeal = MealModel.init(name: "Zero", photo: nil, rating: 0)
+        XCTAssertNotNil(zeroRatingMeal)
+        
+        // Highest positive rating
+        let positiveRatingMeal = MealModel.init(name: "Positive", photo: nil, rating: 5)
+        XCTAssertNotNil(positiveRatingMeal)
+        
     }
     
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
+    func testMealModelInitializationFails(){
+        // Rating exceeds maximum
+        let largeRatingMeal = MealModel.init(name: "Large", photo: nil, rating: 6)
+        XCTAssertNil(largeRatingMeal)
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
+//    override func setUp() {
+//        super.setUp()
+//        // Put setup code here. This method is called before the invocation of each test method in the class.
+//    }
+//    
+//    override func tearDown() {
+//        // Put teardown code here. This method is called after the invocation of each test method in the class.
+//        super.tearDown()
+//    }
+//    
+//    func testExample() {
+//        // This is an example of a functional test case.
+//        // Use XCTAssert and related functions to verify your tests produce the correct results.
+//    }
+//    
+//    func testPerformanceExample() {
+//        // This is an example of a performance test case.
+//        self.measure {
+//            // Put the code you want to measure the time of here.
+//        }
+//    }
     
 }
